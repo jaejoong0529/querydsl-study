ab1514d46309519fe8949d22591a2944ad9e1084
kimjaejoong <wcn07003@naver.com>
2025-12-04 15:04:27 +0900

    [#5] feat(member): Repository 테스트 (QueryDSL 기능 검증)

---
+++ src/test/java/com/querydslstudy/QuerydslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
